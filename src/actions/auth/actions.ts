2834de768a38adfe4a8f1ae3783af16d4bdcbd8d
allen286 <ascend286@gmail.com>
2024-12-29 19:30:08 +0800

    feat: init todo list

---
+++ src/actions/auth/actions.ts
@@ -0,0 +1,57 @@
+"use server"
+
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
+import { createClient } from "@/utils/supabase/server"
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient()
+
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  }
+
+  const { data: resData, error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    console.log('sign ip error', error)
+    throw new Error(error.message)
+  }
+  console.log('sign up data:', resData, 'error:', error)
+
+  revalidatePath("/", "layout")
+  redirect("/signin")
+}
+
+export async function signin(formData: FormData) {
+  const supabase = await createClient()
+
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    console.log('sign in error', error)
+    throw new Error(error.message)
+  }
+
+  revalidatePath("/", "layout")
+  redirect("/")
+}
+
+export async function signout() {
+  const supabase = await createClient()
+
+  let { error } = await supabase.auth.signOut()
+
+  if (error) {
+    console.log('sign out error', error)
+    throw new Error(error.message)
+  }
+
+  redirect("/signin")
+}
---
+++ src/app/api/auth/confirm/route.ts
@@ -0,0 +1,30 @@
+import { type EmailOtpType } from '@supabase/supabase-js'
+import { type NextRequest } from 'next/server'
+
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type') as EmailOtpType | null
+  const next = searchParams.get('next') ?? '/'
+
+  if (token_hash && type) {
+    const supabase = await createClient()
+
+    const { error, data } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    })
+    if (!error) {
+      console.log('verifyOtp data', data)
+      // redirect user to specified redirect URL or root of app
+      redirect(next)
+    }
+    console.log('verifyOtp error', error)
+  }
+
+  // redirect the user to an error page with some instructions
+  redirect('/error')
+}
